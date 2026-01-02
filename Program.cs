1e773af3fc4f83a225309cba98db4d394c16c2b5
Indu <Indu@INDULAPTOP-9949>
2026-01-02 17:41:22 +0530

    commit chsnged

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace newproject
         static void Main(string[] args)
         {
             Console.WriteLine("hell wordes");
-            Console.ReadLine();
+           
         }
     }
 }
